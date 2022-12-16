61d4ffb33677503a12d8feeb6ed4427374363925
ProgHubng <elit4596@gmail.com>
2022-12-16 07:11:09 -0800

    0x04 functions nested loops task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - prints Buss each numbers of 3 and 5
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBizz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	printf('\n');
+	return (0);
+}
