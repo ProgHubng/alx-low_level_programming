18fed916336c388aeba51c7776efd6b49187e503
ProgHubng <elit4596@gmail.com>
2022-12-15 21:45:56 -0800

    0x04 more functions nested loops task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return
+ */
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		if (j != 50 && j != 32)
+		_putchar(j);
+		}
+		_putchar('\n');
+}
