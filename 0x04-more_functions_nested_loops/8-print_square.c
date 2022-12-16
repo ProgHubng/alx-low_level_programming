8c49088248aadea136dfc679aff3cdc9c6083aa8
ProgHubng <elit4596@gmail.com>
2022-12-16 06:21:38 -0800

    0x04 more function nested loops task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints hashes squares
+ * @size: size of the sqaure
+ * Return: not return
+ */
+void print_square(int size)
+{
+	int i, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (k = 0; k < size; k++)
+		{
+			_putchar(35);
+		}
+		if (i != size - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
