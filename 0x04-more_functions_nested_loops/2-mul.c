afdf23bc166cd4974e8221ecb6884323f9258e07
ProgHubng <elit4596@gmail.com>
2022-12-17 16:34:32 -0800

    0x04 more function nested loops task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - print multipication of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: multipication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
