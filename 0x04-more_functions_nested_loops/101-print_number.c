81832b5104fb542d49d101afd543995771c19f22
ProgHubng <elit4596@gmail.com>
2022-12-17 16:14:38 -0800

    0x04 more function nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,8 +6,8 @@
  * Return: no return
  */
 void print_number(int i)
-{
-	unsigned int n, k, counter;
+{ 
+	unsigned int n, k, count;
 
 	if (i < 0)
 	{
@@ -20,16 +20,16 @@ void print_number(int i)
 	}
 
 	k = n;
-	counter = 1;
+	count = 1;
 
 	while (k > 9)
 	{
 		k /= 10;
-		counter *= 10;
+		count *= 10;
 	}
 
-	for (; counter >= 1; counter /= 10)
+	for (; count >= 1; count /= 10)
 	{
-		_putchar(((m / counter) % 10) + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
