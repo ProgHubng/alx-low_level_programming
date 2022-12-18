37b53386d8b35f98070eec29aa119e92ea259c0a
ProgHubng <elit4596@gmail.com>
2022-12-17 17:21:03 -0800

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,28 +9,20 @@ int main(void)
 	int i;
 
 	i = 1;
-	
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-		
 			printf("FizzBizz");
-		
 		else if (i % 3 == 0)
-		
 			printf("Fizz");
-		
 		else if (i % 5 == 0)
-		
 			printf("Buzz");
-		
 		else
-		
 			printf("%i", i);
-		
 		if (i < 100)
 			printf(" ");
 	}
 	printf('\n');
 	return (0);
+
 }
