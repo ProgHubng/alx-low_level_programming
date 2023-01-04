eaae62ab61763400a7b887530ff434b8ce51e1ac
ProgHubng <elit4596@gmail.com>
2023-01-04 08:27:47 -0800

    main file update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,13 @@
 /**
  * _puts_recursion - Prints a string followed by a new line
  * @s: the string to be printed
+ * Return: Always 0 (success).
  */
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		putchar(*s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 
