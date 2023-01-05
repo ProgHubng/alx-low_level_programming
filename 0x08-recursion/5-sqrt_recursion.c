05c4e82845cc6c0bc6221610fa3076ffdda2b69e
ProgHubng <elit4596@gmail.com>
2023-01-04 19:08:42 -0800

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _evaluate - evaluate function sqrt
+ * @i: integer
+ * @n: ingeger
+ * Return: evaluate sqrt
+ */
+int _evaluate(int i, int n)
+{
+	if (n == 0 || n == 1)
+
+		return (n);
+
+	else if (i * i < n)
+
+		return (_evaluate(i + 1, n));
+
+	else if (i * i == n)
+
+		return (i);
+	else
+		return (-1);
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of number
+ * @n: number to find sqrt of
+ * Return: squareroot of n
+ * -1 if n does not have a naturak sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (_evaluate(n, 0));
+}
